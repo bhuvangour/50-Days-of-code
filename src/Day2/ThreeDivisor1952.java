defa98c3b4ede0e66c8e7e1c204500c37308f30d
bhuva <bhuva@LAPTOP-8209EPOI>
2023-12-26 23:19:52 +0530

    Day2

---
+++ src/Day2/ThreeDivisor1952.java
@@ -0,0 +1,16 @@
+package Day2;
+public class ThreeDivisor1952 {
+    public static boolean isThree(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        return count == 3;
+    }
+    public static void main(String[] args){
+    System.out.println(isThree(4));
+    }
+}
