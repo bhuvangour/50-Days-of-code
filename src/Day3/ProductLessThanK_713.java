8aaddf407fe16e8d7ba42f48e5a74b24ff6b267b
bhuva <bhuva@LAPTOP-8209EPOI>
2023-12-27 23:31:09 +0530

    initial push

---
+++ src/Day3/ProductLessThanK_713.java
@@ -0,0 +1,24 @@
+package Day3;
+public class ProductLessThanK_713 {
+	class Solution {
+	    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+
+	        if (k == 0) return 0;
+	        int count = 0;
+
+	        for (int i = 0; i < nums.length; i++) {
+	            long mul = 1;
+	            for (int j = i; j < nums.length; j++) {
+	                mul *= nums[j];
+	                if (mul < k) count++;
+	                else break;
+	            }
+	        }
+	        return count;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(numSubarrayProductLessThanK(new int[]{1, 2, 3, 4}, 100));
+	    }
+    }
+}
