b338624709b276deab1573129ace4bf7027daa8f
bhuva <bhuva@LAPTOP-8209EPOI>
2023-12-27 23:31:32 +0530

    initial push

--- src/Day3/ProductOfArrayExceptSelfDone_238.java
+++ src/Day3/ProductOfArrayExceptSelfDone_238.java
@@ -1,5 +1,30 @@
 package Day3;
 
-public class ProductOfArrayExceptSelfDone_238 {
+import java.util.Arrays;
 
+public class ProductOfArrayExceptSelfDone_238 {
+	public static int[] productExceptSelf(int[] nums) {
+
+        int prefixProduct = 1;
+        int suffixProduct = 1;
+
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = prefixProduct;
+            prefixProduct *= nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
+    }
 }
