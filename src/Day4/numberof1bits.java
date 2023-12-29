94d8a007a320eb81302c1bf12432283d92084e76
bhuva <bhuva@LAPTOP-8209EPOI>
2023-12-29 21:57:33 +0530

    Day->done

--- src/Day4/numberof1bits.java
+++ src/Day4/numberof1bits.java
@@ -1,5 +1,20 @@
 package Day4;
-
+import java.util.*;
 public class numberof1bits {
+	 public static int hammingWeight(int n) {
 
+	        int count = 0;
+	        while (n != 0) {
+	            n = n & (n - 1);    //** as & is true only when both are true
+	            count++;
+	        }
+	        return count;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int n = scanner.nextInt();
+
+	        System.out.println(hammingWeight(n));
+	    }
 }
