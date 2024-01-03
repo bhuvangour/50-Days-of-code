244de56949fe75cdc3d26b3db1dfcad5a4f0ec27
bhuvangour <bhuvangour990@gmail.com>
2024-01-03 23:00:02 +0530

    Day10->Done

---
+++ src/Day10/arithmeticTriplets_2367.java
@@ -0,0 +1,23 @@
+package Day10;
+
+public class arithmeticTriplets_2367 {
+	public static int arithmeticTriplets(int[] nums, int diff) {
+
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[k] - nums[j] == diff && nums[j] - nums[i] == diff) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arithmeticTriplets(new int[]{1, 2, 3, 4, 5}, 3));
+    }
+}
