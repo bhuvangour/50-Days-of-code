87f3f56e5b27d6d52889c9a7ae85cd0a38bce915
bhuvangour <bhuvangour990@gmail.com>
2024-01-05 23:29:59 +0530

    Day12->Done

---
+++ src/Day12/kidsWithCandies_1431.java
@@ -0,0 +1,27 @@
+package Day12;
+import java.util.*;
+public class kidsWithCandies_1431 {
+	public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int maxInititalCandies = 0;
+
+        for (int candy : candies) {
+            maxInititalCandies = Math.max(maxInititalCandies, candy);
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxInititalCandies) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kidsWithCandies(new int[]{2,4,5,6,3}, 5));
+    }
+    }
