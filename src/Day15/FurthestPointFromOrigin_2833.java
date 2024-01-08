d40a2cbc93963c7360719fbe3b09197c6fd6764a
bhuvangour <bhuvangour990@gmail.com>
2024-01-08 22:02:08 +0530

    Day15->Done

---
+++ src/Day15/FurthestPointFromOrigin_2833.java
@@ -0,0 +1,29 @@
+package Day15;
+import java.util.*;
+public class FurthestPointFromOrigin_2833 {
+	public static int furthestDistanceFromOrigin(String moves) {
+
+        int countLeft = 0, countRight = 0, countSpace = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+
+            if (moves.charAt(i) == 'L') countLeft++;
+
+            else if (moves.charAt(i) == 'R') countRight++;
+
+            else countSpace++;
+        }
+
+        if (countRight > countLeft)
+            return countRight + countSpace - countLeft;
+
+        return countLeft + countSpace - countRight;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String moves = scanner.nextLine();
+        System.out.println(furthestDistanceFromOrigin(moves));
+    }
+}
--- src/Day9/fdfh.java
+++
@@ -1,7 +0,0 @@
-package Day9;
-
-public class fdfh {
-public static void main(String[] args) {
-	System.out.println("afaf");
-}
-}
