fe609fb82c568791af98ef81540ef4d8b31ebd69
bhuvangour <bhuvangour990@gmail.com>
2024-01-14 15:02:13 +0530

    Day21->Done

---
+++ src/Day21/FindTheMiddleIndexInArray_1991.java
@@ -0,0 +1,27 @@
+package Day21;
+
+public class FindTheMiddleIndexInArray_1991 {
+	 public static int findMiddleIndex(int[] nums) {
+
+	        if (nums.length < 1)
+	            return -1;
+
+	        int rightSum = 0, leftSum = 0;
+
+	        for (int x : nums)
+	            rightSum += x;
+
+	        for (int x = 0; x < nums.length; x++) {
+	            rightSum -= nums[x];
+	            if (rightSum == leftSum)
+	                return x;
+	            leftSum += nums[x];
+	        }
+
+	        return -1;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(findMiddleIndex(new int[]{1, 6, -3, 8, 4}));
+	    }
+}
