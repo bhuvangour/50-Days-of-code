01cc1b8442fe6d0601ceee6b78215e2fb125f713
bhuvangour <bhuvangour990@gmail.com>
2024-01-16 23:38:17 +0530

    Day23->Done

---
+++ src/Day23/ConvertANumberToHexadecimal_405.java
@@ -0,0 +1,12 @@
+package Day23;
+
+public class ConvertANumberToHexadecimal_405 {
+	    public static String toHex(int num) {
+
+	        return Integer.toHexString(num);
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(toHex(26));
+	    }
+}
