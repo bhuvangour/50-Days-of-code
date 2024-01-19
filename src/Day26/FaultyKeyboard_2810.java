33e8404f72414b35ca9a02156f05e127369f619e
bhuvangour <bhuvangour990@gmail.com>
2024-01-19 23:56:55 +0530

    Day26->Done

---
+++ src/Day26/FaultyKeyboard_2810.java
@@ -0,0 +1,21 @@
+package Day26;
+
+public class FaultyKeyboard_2810 {
+	 public static String finalString(String s) {
+
+	        StringBuilder ans = new StringBuilder();
+
+	        for (int i = 0; i < s.length(); i++) {
+	            if (s.charAt(i) == 'i') {
+	                ans.reverse();
+	            } else {
+	                ans.append(s.charAt(i));
+	            }
+	        }
+	        return String.valueOf(ans);
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(finalString("string"));
+	    }
+}
