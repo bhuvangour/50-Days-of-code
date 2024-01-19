0772676569ace352ec6a5a8a2f9f3184f8d331d2
bhuvangour <bhuvangour990@gmail.com>
2024-01-19 23:55:02 +0530

    Day26->Done

---
+++ src/Day26/CountNegativeNumbersInASortedMatrix_135.java
@@ -0,0 +1,23 @@
+package Day26;
+
+public class CountNegativeNumbersInASortedMatrix_135 {
+	   public static int countNegatives(int[][] grid) {
+
+	        int count = 0;
+	        int col = grid[0].length;
+
+	        for (int[] ints : grid) {
+	            for (int j = 0; j < col; j++) {
+	                if (ints[j] < 0) {
+	                    count++;
+	                }
+	            }
+	        }
+	        return count;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(countNegatives(new int[][]{{1, 2, 3, 4, 5}, {-4, -8, 6, -5, 3}}));
+	    
+}
+	    }
