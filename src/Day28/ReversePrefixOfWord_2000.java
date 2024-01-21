6fa4256a7cbcae2eef82780adeb34317d373fa90
bhuvangour <bhuvangour990@gmail.com>
2024-01-21 22:59:33 +0530

    Day28->Done

---
+++ src/Day28/ReversePrefixOfWord_2000.java
@@ -0,0 +1,29 @@
+package Day28;
+
+public class ReversePrefixOfWord_2000 {
+	public static String reversePrefix(String word, char ch) {
+
+        StringBuilder ans = new StringBuilder();
+
+        if (word.contains(String.valueOf(ch))) {
+
+            int rev = word.indexOf(ch);
+
+            for (int i = rev; i >= 0; i--) {
+                ans.append(String.valueOf(word.charAt(i)));
+            }
+
+            ans.append(word.substring(rev + 1));
+
+            return ans.toString();
+
+        } else
+            return word;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdef", 'd'));
+    }
+
+}
