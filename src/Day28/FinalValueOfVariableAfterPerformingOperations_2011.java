7d326fad8b3b2ed63a9621726de6e13d9aa2fc54
bhuvangour <bhuvangour990@gmail.com>
2024-01-21 23:01:08 +0530

    Day28->Done

---
+++ src/Day28/FinalValueOfVariableAfterPerformingOperations_2011.java
@@ -0,0 +1,23 @@
+package Day28;
+
+public class FinalValueOfVariableAfterPerformingOperations_2011 {
+	public static int finalValueAfterOperations(String[] operations) {
+
+        int sum = 0;
+
+        for (String operation : operations) {
+
+            if (operation.equals("++X") || operation.equals("X++")) {
+                sum++;
+            } else {
+                sum--;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(finalValueAfterOperations(new String[]{"--X", "X++", "X++"}));
+    }
+}
