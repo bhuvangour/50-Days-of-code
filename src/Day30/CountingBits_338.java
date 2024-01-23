6f4bb9ee32db68735ca989e7dde86ece572bad55
bhuvangour <bhuvangour990@gmail.com>
2024-01-23 21:44:37 +0530

    Day30->Done

---
+++ src/Day30/CountingBits_338.java
@@ -0,0 +1,28 @@
+package Day30;
+import java.util.*;
+public class CountingBits_338 {
+	 public static int[] countBits(int n) {
+
+	        int[] ans = new int[n + 1];
+
+	        for (int i = 0; i <= n; i++) {
+	            String str = Integer.toBinaryString(i);
+	            int count = 0;
+
+	            for (int j = 0; j < str.length(); j++) {
+	                if (str.charAt(j) == '1') {
+	                    count++;
+	                }
+	            }
+	            ans[i] = count;
+	        }
+	        return ans;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int n = scanner.nextInt();
+
+	        System.out.println(Arrays.toString(countBits(n)));
+	    }
+}
