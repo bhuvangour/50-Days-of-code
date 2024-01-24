ab9febf23fef038c50a427e0ce4e8cdc53f924bd
bhuvangour <bhuvangour990@gmail.com>
2024-01-24 22:40:50 +0530

    Day31->Done

---
+++ src/Day31/CountSubarraysWithScoreLessThanK_2302.java
@@ -0,0 +1,20 @@
+package Day31;
+
+public class CountSubarraysWithScoreLessThanK_2302 {
+	public static long countSubarrays(int[] A, long k) {
+
+        long ans = 0, cur = 0;
+
+        for (int j = 0, i = 0; j < A.length; ++j) {
+            cur += A[j];
+            while (cur * (j - i + 1) >= k)
+                cur -= A[i++];
+            ans += j - i + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSubarrays(new int[]{1, 2, 6, 3, 5}, 10));
+    }
+}
