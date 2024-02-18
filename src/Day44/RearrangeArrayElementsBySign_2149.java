7b299ab26f1c29731635c881691423e3b6f8ffb4
bhuvangour <bhuvangour990@gmail.com>
2024-02-18 08:21:43 +0530

    Day44->Done

---
+++ src/Day44/RearrangeArrayElementsBySign_2149.java
@@ -0,0 +1,24 @@
+package Day44;
+
+public class RearrangeArrayElementsBySign_2149 {
+	 public int[] rearrangeArray(int[] nums) {
+	        // code here
+	        int[] ans = new int[nums.length];
+	        int pos = 0, neg = 1;
+	        
+	        for (int i = 0; i < nums.length; i++) {
+	            
+	            if (nums[i] > 0) {
+	                ans[pos] = nums[i];
+	                pos += 2;
+	            } 
+	            
+	            else {
+	                ans[neg] = nums[i];
+	                neg += 2;
+	            }
+	        }
+	        
+	        return ans;
+	    }
+}
