280247884b23d5ba84f5989a5215c238e4ce2338
bhuvangour <bhuvangour990@gmail.com>
2024-02-25 23:12:18 +0530

    Day58->Done

---
+++ src/Day58/RemoveDuplicatesfromSortedArrayII_81.java
@@ -0,0 +1,20 @@
+package Day58;
+
+public class RemoveDuplicatesfromSortedArrayII_81 {
+    public int removeDuplicates(int[] nums) {
+        
+        int count = 2;
+        
+        for (int i = 2; i < nums.length; i++) {
+            
+            if (nums[count - 2] != nums[i]) {
+                
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
